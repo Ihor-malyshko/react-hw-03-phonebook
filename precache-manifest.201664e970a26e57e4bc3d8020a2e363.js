a9696e12c5b1074d6070a9e2e6a52c35983f7ca1
ihor <malishkoio@gmail.com>
2020-09-11 02:44:46 +0300

    Updates

---
+++ precache-manifest.201664e970a26e57e4bc3d8020a2e363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7d2406c41a57b34016d21869063203",
+    "url": "/react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "d4d588205e73b1227083",
+    "url": "/react-hw-03-phonebook/static/js/2.96800ae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-03-phonebook/static/js/2.96800ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96aa80acb05b9996533d",
+    "url": "/react-hw-03-phonebook/static/js/main.ef460151.chunk.js"
+  },
+  {
+    "revision": "3553964e198b065fb72b",
+    "url": "/react-hw-03-phonebook/static/js/runtime-main.2a925baa.js"
+  }
+]);
