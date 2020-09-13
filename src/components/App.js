fb9b81c33aa5a2b25c51d21a1d8e353c961f8e7b
ihor <malishkoio@gmail.com>
2020-09-13 11:01:38 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,32 @@ import ContactForm from './ContactForm';
 import Filter from './Filter';
 import { v4 as uuidv4 } from 'uuid';
 
+const defContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 class Phonebook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [{ id: '', name: '', number: '' }],
     filter: '',
   };
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem('contacts');
+    if (localContacts === null) {
+      localStorage.setItem('contacts', JSON.stringify(defContacts));
+    }
+    this.setState({ contacts: JSON.parse(localContacts) });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     this.setState(prevState => {
@@ -52,6 +68,7 @@ class Phonebook extends Component {
   render() {
     return (
       <>
+        <h2>hw 03</h2>
         <Section title="Phonebook">
           <ContactForm onAddContact={this.addContact} />
           {/* <ContactEditor onAddContact={this.addContact} /> */}
